4b026a72e57782c3fd65319a30115f8b1695f64b
rizki <rizkiadnanfutuh@gmail.com>
2024-12-07 07:05:18 +0700

    feat: add messages screen with Firebase integration

--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -1,10 +1,217 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class MessagesScreen extends StatelessWidget {
+import '../components/my_icon_button.dart';
+
+class MessagesScreen extends StatefulWidget {
   const MessagesScreen({super.key});
 
   @override
+  State<MessagesScreen> createState() => _MessagesScreenState();
+}
+
+class _MessagesScreenState extends State<MessagesScreen> {
+  final CollectionReference messageCollection =
+      FirebaseFirestore.instance.collection("MyMessage");
+  List<String> messagesScreenType = ["All", "Traveling", "Support"];
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.black,
+        child: const Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.white,
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  const SizedBox(),
+                  const Spacer(),
+                  MyIconButton(
+                    radius: 23,
+                    icon: Icons.search,
+                    bgIconColor: Colors.black12.withOpacity(0.03),
+                  ),
+                  const SizedBox(width: 10),
+                  MyIconButton(
+                    radius: 23,
+                    icon: Icons.tune,
+                    bgIconColor: Colors.black12.withOpacity(0.03),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                "Messages",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    messagesScreenType.length,
+                    (index) => GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 22,
+                            vertical: 12,
+                          ),
+                          decoration: BoxDecoration(
+                            color: selectedIndex == index
+                                ? Colors.black
+                                : Colors.black12.withOpacity(0.04),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text(
+                            messagesScreenType[index],
+                            style: TextStyle(
+                              color: selectedIndex == index
+                                  ? Colors.white
+                                  : Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Expanded(child: _buildMessageItem()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  StreamBuilder<QuerySnapshot<Object?>> _buildMessageItem() {
+    return StreamBuilder(
+        stream: messageCollection.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                padding: const EdgeInsets.only(top: 20),
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final msg = snapshot.data!.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Row(
+                      children: [
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(
+                              height: 85,
+                              width: 75,
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(msg['image']),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: -10,
+                              right: -18,
+                              child: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(msg['vendorImage']),
+                                backgroundColor: Colors.white,
+                                radius: 25,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 20),
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      msg['name'],
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.black87,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    msg['date'],
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                msg['description'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                msg['duration'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
